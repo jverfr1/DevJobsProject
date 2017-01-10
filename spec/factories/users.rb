d2c02a9157c313f551571ddbbc33adab2c1cda48
jverfr1 <jverfr1@gmail.com>
2017-01-10 09:50:51 +0100

    Test working

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,22 @@
 FactoryGirl.define do
+    # sequence :name do |n| "person#{n}" end
+    # sequence :last_name do|n| "Last name #{n}" end
+    # sequence :experience do|n| "#{n} years with rails"end
+
   factory :user do
-    
+    # name {generate(:name)}
+    # last_name {generate(:last_name)}
+    # position "junior"
+    # experience {generate(:experience)}
+    # sequence(:email) {|n| "person#{n}@example.com"}
+    # passwword(:password) {rand(99999..100000)}
+    # name "Jonathan"
+    email "jonathan@example.com"
+    password "password"
+    password_confirmation "password"
+    name "jonathan"
+    last_name "veron"
+    position "junior"
+    experience "Five years"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
+# require '<rails></rails>_helper'
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {build(:user)}
+    it "name is string" do
+      expect(user.name.class =="String").to be_truth
+    end
 end
